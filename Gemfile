dad6e76d4916a1a9aa4fce9b9244d802863265db
Tim Renken <tim@timrenken.io>
2015-10-18 06:55:41 -0500

    added connections and twitter omniauth

--- Gemfile
+++ Gemfile
@@ -33,3 +33,4 @@ gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootst
 gem "font-awesome-rails"
 gem 'simple_form'
 gem 'devise'
+gem 'omniauth-twitter'
---
+++ app/controllers/connections_controller.rb
@@ -0,0 +1,28 @@
+class ConnectionsController < ApplicationController
+  before_action :set_connection, only: [:destroy]
+  
+  def create
+    connection = current_user.connections.create_from_omniauth(auth_hash)
+    if connection.save
+      redirect_to dashboard_path, notice: "Connection created!"
+    else
+      redirect_to dashboard_path, notice: "Oops! Something went wrong!"
+    end
+  end
+
+  def destroy
+    @connection.destroy
+    redirect_to dashboard_path, notice: "Connection removed!"
+  end
+  
+  private
+  
+  def set_connection
+    @connection = Connection.find(params[:id])
+  end
+  
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+  
+end
