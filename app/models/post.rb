68414b2c08c95dd0d47419f619bc8ba575d87c34
Tim Renken <tim@timrenken.io>
2015-10-18 19:26:07 -0500

    added further time validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates_presence_of :content
   validates_presence_of :scheduled_at
   validates_length_of :content, maximum: 140, messages: "Needs to be less than 140 characters"
+  validates_datetime :scheduled_at, :on => :create, :on_or_after => Time.zone.now
 end
